5b32871b415477d070fd0f8ac01386deab401549
Andrey Vladykov <avladykov@outlook.com>
2020-07-14 13:34:27 +0300

    Use IdGenerator in the FileUploadController.

--- api/src/main/java/com/instahost/api/controller/FileUploadController.java
+++ api/src/main/java/com/instahost/api/controller/FileUploadController.java
@@ -1,19 +1,41 @@
 package com.instahost.api.controller;
 
+import com.instahost.api.service.IdGenerator;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileOutputStream;
 
 @RestController
 @RequestMapping("/api/files")
 @Slf4j
 public class FileUploadController {
+
+    private final IdGenerator idGenerator;
+
+    public FileUploadController(IdGenerator idGenerator) {
+        this.idGenerator = idGenerator;
+    }
+
+    @SneakyThrows
     @PostMapping
-    public ResponseEntity upload() {
+    public ResponseEntity upload(@RequestParam("file") MultipartFile file) {
         log.info("Got an upload request.");
+
+        var bytes = file.getBytes();
+
+        var id = idGenerator.generate();
+
+        var f = new FileOutputStream(id);
+        f.write(bytes);
+        f.close();
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
--- api/src/test/java/com/instahost/api/controller/FileUploadControllerTest.java
+++ api/src/test/java/com/instahost/api/controller/FileUploadControllerTest.java
@@ -1,15 +1,20 @@
 package com.instahost.api.controller;
 
+import com.instahost.api.service.IdGenerator;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(FileUploadController.class)
 class FileUploadControllerTest {
@@ -17,13 +22,25 @@ class FileUploadControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private IdGenerator idGenerator;
+
     @SneakyThrows
     @Test
     void postFile() {
-        mvc.perform(post("/api/files"))
+        // given
+        var id = "12345abc";
+        given(idGenerator.generate()).willReturn(id);
+
+        // when
+        var file = new MockMultipartFile("file", "foo.bar", "text/plain", "body".getBytes());
+
+        var response = mvc.perform(multipart("/api/files").file(file))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string("uploaded"))
-        ;
+                .andReturn()
+                .getResponse();
+
+        // then
+        assertThat(response.getStatus(), is(HttpStatus.CREATED.value()));
     }
 }
