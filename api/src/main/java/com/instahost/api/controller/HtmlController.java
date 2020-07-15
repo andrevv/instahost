8c42c48914851cd05a8152c0cb8c42328258cc2c
Andrey Vladykov <avladykov@outlook.com>
2020-07-15 23:18:03 +0300

    Use FileStorage in HtmlController to retrieve files.

--- api/src/main/java/com/instahost/api/controller/HtmlController.java
+++ api/src/main/java/com/instahost/api/controller/HtmlController.java
@@ -1,5 +1,6 @@
 package com.instahost.api.controller;
 
+import com.instahost.api.service.FileStorage;
 import lombok.SneakyThrows;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -8,13 +9,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 @Controller
 @RequestMapping("/")
 public class HtmlController {
+
+    private final FileStorage fileStorage;
+
+    public HtmlController(FileStorage fileStorage) {
+        this.fileStorage = fileStorage;
+    }
+
     @SneakyThrows
     @GetMapping(value = "/{id}", produces = MediaType.TEXT_HTML_VALUE)
     @ResponseBody
     public String getHtml(@PathVariable String id) {
-        return id;
+        var file = fileStorage.retrieve(id);
+
+        return new String(file, UTF_8);
     }
 }
--- api/src/test/java/com/instahost/api/controller/HtmlControllerTest.java
+++ api/src/test/java/com/instahost/api/controller/HtmlControllerTest.java
@@ -1,11 +1,16 @@
 package com.instahost.api.controller;
 
+import com.instahost.api.service.FileStorage;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -17,18 +22,24 @@ class HtmlControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private FileStorage fileStorage;
+
     @SneakyThrows
     @Test
     void getHtml() {
         // given
         final String id = "12345abc";
+        final String file = "file data";
+        given(fileStorage.retrieve(id)).willReturn(file.getBytes());
 
         // when
         mvc.perform(get("/" + id))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(id));
+                .andExpect(content().string(file));
 
         // then
+        verify(fileStorage, times(1)).retrieve(id);
     }
 }
